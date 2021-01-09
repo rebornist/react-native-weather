01b91a17167737ff3ff6878a84186c426dc94c8c
sshs0702 <rebornist@naver.com>
2021-01-09 15:39:12 +0900

    create Loading page

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white'
-  }
-});
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.yellowView}>
+//         <Text>Hello!</Text>
+//       </View>
+//       <View style={styles.blueView}>
+//         <Text>Hello!</Text>
+//       </View>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   yellowView: {
+//     flex: 1,
+//     backgroundColor: 'yellow',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   blueView: {
+//     flex: 1,
+//     backgroundColor: 'blue',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
